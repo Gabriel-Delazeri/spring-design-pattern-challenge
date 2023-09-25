4743af516ad39311d406102b1e0c3ecf07a79c7b
Gabriel-Delazeri <gbldelazeri@gmail.com>
2023-09-25 11:47:54 -0300

    feat: first commit

---
+++ src/test/java/com/delazeri/springdesignpatterns/SpringDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delazeri.springdesignpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDesignPatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
